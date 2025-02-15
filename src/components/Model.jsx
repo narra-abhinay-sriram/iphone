5d61f766bb6f710ee92745fe0c37837b9aa802fe
narra-abhinay-sriram <121sriramreddy@gmail.com>
2025-02-15 13:49:36 +0530

    3d rendering using three js

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,13 @@
 import { useGSAP } from '@gsap/react'
 import {gsap} from 'gsap'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ModelView from './ModelView'
 import { yellowImg } from '../utils'
 import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { View } from '@react-three/drei'
 import { models, sizes } from '../constants'
+import { gsapanimation } from '../utils/animations'
 
 const Model = () => {
     const [size,setSize] = useState('small')
@@ -15,6 +16,23 @@ const Model = () => {
     color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
     img: yellowImg,
     })
+ const tl = gsap.timeline();
+ useEffect(()=>{
+    if(size=='small')
+    {
+        gsapanimation(tl,large,largeRotation,'#view2','#view1',{
+            transform :'translateX(0)',
+            duration:1
+        })
+    }
+    if(size=='large')
+    {
+        gsapanimation(tl,small,smallRotation,'#view1','#view2',{
+            transform :'translateX(-100%)',
+            duration:2
+        })
+    }
+ },[size])
 
   const camControlSmall = useRef();
   const camControlLarge = useRef();
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-
-const ModelView = () => {
+import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Lights from './Lights'
+import Iphone from './Iphone'
+import * as three from 'three'
+const ModelView = ({index,groupRef,gsapType,controlRef,setRotationState,item,size}) => {
   return (
-    <div>
-      modelview
-    </div>
+    <View
+    index={index}
+    id={gsapType}
+    className={` w-full h-full absolute ${index==2 ? 'right-[-100%]': ''}`}
+    >
+  <ambientLight intensity={5} />
+  <PerspectiveCamera makeDefault position={[0,0,4]} />
+  <Lights/>
+  <OrbitControls
+  makeDefault
+  ref={controlRef}
+  enableZoom={false}
+  enablePan={false}
+  rotateSpeed={0.4}
+  target={new three.Vector3(0,0,0)}
+  onEnd={()=> setRotationState(controlRef.current.getAzimuthalAngle())}
+   />
+  <group ref={groupRef} name={index==1 ? 'small' : 'large'} position={[0,0,1]} >
+  <Suspense  fallback={<Html><div>Loading...</div></Html>}>
+  <Iphone
+  scale={index===1 ? [15,15,15] : [17,17,17]}
+  item={item}
+  size={size}
+  />
+
+  </Suspense>
+  </group>
+  
+    </View>
   )
 }
 
